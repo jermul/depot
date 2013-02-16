16ef7e2a88d962891cecaec953273479518f038a
Jeremy Muljana <jermul714@gmail.com>
2013-02-16 01:20:41 -0800

    Ch14 finished

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,8 +1,9 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :product, :product_id, :id, :updated_at, :cart_id, :created_at, :cart
+  attr_accessible :cart_id, :product_id, :order_id, :updated_at, :created_at, :quantity, :cart, :product, :price
   belongs_to :order
   belongs_to :product
   belongs_to :cart
+  
   def total_price 
   	product.price * quantity
   end
